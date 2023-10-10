06c31d394c8de30e6a2b0840dd5294fe5b528bc4
Srinivasarao Maddila <maddila.srinivasa@cloudeq.com>
2023-10-10 18:03:27 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export const App = () => {
           href="https://www.youtube.com/channel/UCJV16_5c4A0amyBZSI4yP6A"
           target="_blank"
         >
-          Matt Sokola
+          Srinivas
         </a>
       </div>
     </div>
